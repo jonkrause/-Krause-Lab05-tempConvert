913ebe3cd59758fa49118eda041263ed7e5109b5
jonkrause <jonathonkrause@gmail.com>
2017-06-30 08:50:35 -0400

    first commit

---
+++ temp.js
@@ -0,0 +1,17 @@
+var fTemp = prompt("Enter the degrees in Fahrenheit you would like to convert");
+console.log((fTemp - 32) * 5 / 9);
+
+
+
+//take a bath, do the math
+
+var fTemp = prompt("Enter the degrees in Fahrenheit you would like to convert");
+console.log((fTemp - 32) * 5 / 9);
+
+console.log("Let\'s do the math!" + " First we\'ll subtract 32 from the number you entered: " + fTemp + " - 32 = " + (fTemp - 32))
+
+var minus32 = (fTemp - 32);
+console.log("Then we\'ll multiply " + minus32 + " by 5, which gives us: " + (minus32 * 5))
+
+var timesFive = (minus32 * 5);
+console.log("Once we\'ve done that we can take " + timesFive + " and divide it by 9 to get our final answer. which is: " + (timesFive / 9))
